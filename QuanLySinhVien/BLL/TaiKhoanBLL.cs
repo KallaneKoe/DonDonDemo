8c761f4e28db149aff25d954b4b0b66fd21f7a5c
KallaneKoe <namnguyenhoang0903@gmail.com>
2022-11-20 12:32:35 +0700

    first commit

---
+++ QuanLySinhVien/BLL/TaiKhoanBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+
+namespace BLL
+{
+    public class TaiKhoanBLL
+    {
+        TaiKhoanAcess tkAccess = new TaiKhoanAcess();
+        public string CheckLogic(TaiKhoan taikhoan)
+        {
+            if (taikhoan.sTaiKhoan == "")
+            {
+                return "requeid_taikhoan";
+            }
+
+            if (taikhoan.sMatKhau == "")
+            {
+                return "requeid_password";
+            }
+
+            string info = tkAccess.CheckLogic(taikhoan);
+            return info;
+        }
+    }
+}
---
+++ QuanLySinhVien/DAL/DatabaseAccess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+
+namespace DAL
+{
+    public class SqlConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+            string strcon = @"Data Source=DESKTOP-DQSEFU7\NGUYENLAM;Initial Catalog=QuanLySinhVien;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strcon); 
+            return conn;
+        }
+    }
+
+    public class DatabaseAccess
+    {
+        public static string CheckLogicDTO(TaiKhoan taikhoan)
+        {
+            string user = null;
+            SqlConnection conn = SqlConnectionData.Connect();
+            conn.Open();
+            SqlCommand command = new SqlCommand("proc_logic", conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@user", taikhoan.sTaiKhoan);
+            command.Parameters.AddWithValue("@pass", taikhoan.sMatKhau);
+            command.Connection = conn;
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    user = reader.GetString(0);
+                }
+                reader.Close();
+                conn.Close();
+            } else
+            {
+                return "Tài khoản hoặc mật khẩu không chính xác!";
+            }
+             
+            return user;
+        }
+    }
+}
